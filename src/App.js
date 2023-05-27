b2bedc51ed3f80e6d211a76a561659677d305ac7
Samriddhi Guha <134717404+samriddhiguha@users.noreply.github.com>
2023-05-27 20:27:52 +0000

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [currW, nextW] = useState('');
+  useEffect(() => {
+    (async () => {
+      const pr = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Kolkata&units=metric&APPID=9723673fa52d46fb73759eb78bdef3b2');
+      const response = await pr.json();
+      nextW(response);
+    })();
+  },[])
+  console.log(currW);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Weather App</h1>
+
+      <h4>Location : Kolkata, India</h4>
+      <h4>Temperature : {currW.main.temp}</h4>
+      <h4>Humidity : {currW.main.humidity}%</h4>
+      <h4>Wind Speed : {currW.wind.speed}</h4>
+      <h4>Description : {currW.weather[0].description}</h4>
+      <p></p>
     </div>
   );
 }
